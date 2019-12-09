8617617b28500d0662a2e8ae0d3cc7a7e990eaaf
Claire Ramos <Bella@Claires-MacBook-Pro.local>
2019-12-09 16:10:47 -0500

    Got the jokes to show up on the screen, prelim api implementation done

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'chuck_norris'
 
 get '/' do
-    joke = ChuckNorris::JokeFinder.get_joke
-    puts joke
+    @joke = ChuckNorris::JokeFinder.get_joke
+    puts @joke.joke
     erb :index
 end
